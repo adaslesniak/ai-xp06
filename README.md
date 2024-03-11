662f87c70e28bdab924b3713bc8e9d5f9d69c5eb
Adas Lesniak <radical.ignorant@gmail.com>
2024-03-11 22:46:20 +0100

    added results to readme

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ of random forest.
 Goal of this experiment is to create alternative implementation of random forest,
 that would use more uniform distribution of features among trees in the forest 
 and check how that would affect results.
+
+# Results
+Unclear, definitely RandomForestClassifier isn't as simple as described in lectures. 
+My naive approach is just terrible compared to it, but my naive approach with 
+the trick of more uniform distribution vs without the trick - difference is huge.
+
+========== IRIS ==========
+classic >>  cv accuracy: 0.95 +/- 0.03,  Accuracy: 1.00, Precision: 1.00, Recall: 1.00, F1 Score: 1.00
+uniform >>  cv accuracy: 0.91 +/- 0.09,  Accuracy: 1.00, Precision: 1.00, Recall: 1.00, F1 Score: 1.00
+naive   >>  cv accuracy: 0.69 +/- 0.31,  Accuracy: 0.56, Precision: 0.46, Recall: 0.54, F1 Score: 0.45
+========== Wine Quality ==========
+classic >>  cv accuracy: 0.59 +/- 0.03,  Accuracy: 0.59, Precision: 0.29, Recall: 0.26, F1 Score: 0.26
+uniform >>  cv accuracy: 0.55 +/- 0.06,  Accuracy: 0.56, Precision: 0.29, Recall: 0.24, F1 Score: 0.24
+naive   >>  cv accuracy: 0.34 +/- 0.05,  Accuracy: 0.32, Precision: 0.17, Recall: 0.23, F1 Score: 0.13
--- test.py
+++ test.py
@@ -15,7 +15,7 @@ def evaluate(model, title, predicates, labels):
     precision, recall, f1_score, _ = precision_recall_fscore_support(test_labels, predictions, average='macro', zero_division=0)
     cv_scores = cross_val_score(model, predicates, labels, cv=5, scoring='accuracy')
     cross_validation_score = f'{cv_scores.mean():.2f} +/- {cv_scores.std():.2f}'
-    result = f"avg accuracy: {cross_validation_score},  Accuracy: {accuracy:.2f}, Precision: {precision:.2f}, Recall: {recall:.2f}, F1 Score: {f1_score:.2f}"
+    result = f"cv accuracy: {cross_validation_score},  Accuracy: {accuracy:.2f}, Precision: {precision:.2f}, Recall: {recall:.2f}, F1 Score: {f1_score:.2f}"
     print(title, ">> ", result)  
 
 
@@ -32,12 +32,10 @@ def compare(data_name, predicates, labels):
     evaluate(naive_forest, "naive  ", predicates, labels)
 
 
-#iris = load_iris()
-#compare("IRIS", iris.data, iris.target)
+iris = load_iris()
+compare("IRIS", iris.data, iris.target)
 
 wine = pd.read_csv('wine/winequality-red.csv', delimiter=';')
-print(wine.info())
-print(wine.shape)
 wine_features = wine.drop('quality', axis=1).values
 wine_labels = wine['quality'].values
 compare("Wine Quality", wine_features, wine_labels)
